9e710dc5a53244b96f7519f96f5feae1e38733b2
Qutom <dlwlstjr0530@naver.com>
2022-06-05 05:10:25 +0900

    Add TestCase for YoutubeMusicLoader

--- src/test/java/com/discord/bot/service/MusicLoaderTest.java
+++ src/test/java/com/discord/bot/service/MusicLoaderTest.java
@@ -3,11 +3,13 @@ package com.discord.bot.service;
 import com.discord.bot.BotApplication;
 import com.discord.bot.entity.pojo.MusicPojo;
 import com.discord.bot.loader.*;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.junit.Before;
-import org.junit.jupiter.api.Assertions.*;
-import org.mockito.Mock;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.SpringApplication;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.MockitoAnnotations.openMocks;
@@ -16,6 +18,8 @@ public class MusicLoaderTest {
 
     @Mock
     RestService restService;
+    @Mock
+    SlashCommandInteractionEvent event;
 
     MusicLoaderManager musicLoaderManager;
 
@@ -30,8 +34,8 @@ public class MusicLoaderTest {
     }
 
     /**
-     * Purpose: Verify that the correct loader is generated for the given URL
-     * Input: URLs
+     * Purpose: Verify the correct MusicLoader is generated for the given URL
+     * Input: URLs to MusicLoaderFactory
      *  String(Youtube Single Track) : https://www.youtube.com/watch?v=em0MknB6wFo
      *  String(Youtube Playlist) : https://www.youtube.com/watch?v=A2VpR8HahKc&list=PLSdoVPM5WnndSQEXRz704yQkKwx76GvPV
      *  String(Spotify Single Track) : https://open.spotify.com/track/2Foc5Q5nqNiosCNqttzHof
@@ -56,6 +60,31 @@ public class MusicLoaderTest {
         assertTrue(spotifySingleLoader instanceof SpotifyMusicLoader);
         assertTrue(spotifyListLoader instanceof SpotifyMusicLoader);
         assertTrue(musicNameLoader instanceof YoutubeMusicLoader);
+    }
 
+    /**
+     * Purpose: Verify YoutubeMusicLoader class generates a list for a given URL
+     * Input: URLs to YoutubeMusicLoader
+     *  String(Youtube Single Track) : https://www.youtube.com/watch?v=em0MknB6wFo
+     *  String(Youtube Playlist) : https://www.youtube.com/watch?v=A2VpR8HahKc&list=PLSdoVPM5WnndSQEXRz704yQkKwx76GvPV
+     * Expected:
+     *  size 1, List<MusicPojo>, first element in this list has same String as Youtube Single Track URL at youtubeUri field
+     *  size 1, List<MusicPojo>, first element in this list has same String as Youtube Playlist URL at youtubeUri field
+     */
+    @Test
+    public void YoutubeMusicLoaderTest() {
+        String singleMusicUrl = "https://www.youtube.com/watch?v=em0MknB6wFo";
+        String musicListUrl = "https://www.youtube.com/watch?v=A2VpR8HahKc&list=PLSdoVPM5WnndSQEXRz704yQkKwx76GvPV";
+        MusicLoader youtubeSingleLoader = MusicLoaderFactory.createMusicLoader(singleMusicUrl);
+        MusicLoader youtubeListLoader = MusicLoaderFactory.createMusicLoader(musicListUrl);
+
+        List<MusicPojo> singleMusic = youtubeSingleLoader.getMusicPojos(restService, singleMusicUrl, event);
+        List<MusicPojo> playlist = youtubeListLoader.getMusicPojos(restService, musicListUrl, event);
+
+        assertEquals(1, singleMusic.size());
+        assertEquals(1, playlist.size());
+
+        assertEquals(singleMusic.get(0).getYoutubeUri(), singleMusicUrl);
+        assertEquals(playlist.get(0).getYoutubeUri(), musicListUrl);
     }
 }
